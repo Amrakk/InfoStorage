b41f9f5676e6ee5efeedd02c36f0bd2fc61e30d4
LTP <lamtruongphu2003@gmail.com>
2023-08-24 23:57:22 +0700

    Update 24-08

--- server/src/api/v1/auth/signin.ts
+++ server/src/api/v1/auth/signin.ts
@@ -4,8 +4,8 @@ import database from "../../../database/db.js";
 import { roles } from "../../../settings/global.js";
 import { publicProcedure } from "../../../trpc.js";
 import {
-    setAccessToken,
-    setRefreshToken,
+  setAccessToken,
+  setRefreshToken,
 } from "../../../middlewares/setToken.js";
 import { valEmail, valPassword } from "../../../middlewares/validateInput.js";
 
@@ -13,33 +13,32 @@ const db = database.getDB();
 const users = db.collection("users");
 
 export const signin = publicProcedure
-    .input(
-        z.object({
-            email: z.string(),
-            password: z.string(),
-        })
-    )
-    .query(async (opts) => {
-        const { email, password } = opts.input;
+  .input(
+    z.object({
+      email: z.string(),
+      password: z.string(),
+    })
+  )
+  .query(async (opts) => {
+    const { email, password } = opts.input;
 
-        if (!email || !password) return null;
-        if (!valEmail(email) || !valPassword(password)) return null;
+    if (!email || !password) return null;
+    if (!valEmail(email) || !valPassword(password)) return null;
 
-        const user = await users.findOne({ email });
-        if (!user) return null;
-        if (!bycrpt.compareSync(password, user.password)) return null;
+    const user = await users.findOne({ email });
+    if (!user) return null;
+    if (!bycrpt.compareSync(password, user.password)) return null;
 
-        const permissions = (
-            roles.find((role) => role.role === user.role) || {}
-        ).permissions;
-        if (!permissions) return null;
-        const tokenData = {
-            id: user._id,
-            permissions: permissions,
-        };
+    const permissions = (roles.find((role) => role.role === user.role) || {})
+      .permissions;
+    if (!permissions) return null;
+    const tokenData = {
+      id: user._id,
+      permissions: permissions,
+    };
 
-        setAccessToken(tokenData, opts.ctx.res);
-        setRefreshToken(tokenData.id, opts.ctx.res);
+    setAccessToken(tokenData, opts.ctx.res);
+    setRefreshToken(tokenData.id, opts.ctx.res);
 
-        return { message: "Signin successfully" };
-    });
+    return { message: "Signin successfully" };
+  });
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,30 +5,31 @@ import database from "./database/db.js";
 import cookieParser from "cookie-parser";
 import { appRouter } from "./routers/appRouter.js";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { createContext } from "./context.js";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: [process.env.DEV_URL ?? "", process.env.PROD_URL ?? ""],
-        credentials: true,
-    })
+  cors({
+    origin: [process.env.DEV_URL ?? "", process.env.PROD_URL ?? ""],
+    credentials: true,
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-app.use("/trpc", createExpressMiddleware({ router: appRouter }));
+app.use("/trpc", createExpressMiddleware({ router: appRouter, createContext }));
 
 app.listen(process.env.PORT, async () => {
-    await cache.init();
-    await database.init();
-    console.log(`Server listening on port ${process.env.PORT}`);
+  await cache.init();
+  await database.init();
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
 
 app.on("close", async () => {
-    await cache.close();
-    await database.close();
-    console.log("Server closed");
+  await cache.close();
+  await database.close();
+  console.log("Server closed");
 });
 
 export type AppRouter = typeof appRouter;
